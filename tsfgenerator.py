5181dc4bad0201ecb5a6f0cb8a46e8c52f6197a0
Steve Brown <sddbrown@gmail.com>
2021-05-31 20:48:13 -0700

    refactored

--- tsfgenerator.py
+++ tsfgenerator.py
@@ -1,18 +1,10 @@
 import sys
 import getpass
-#from myfunctions.generate_dump import generate_dump
 from myfunctions.ip_addr_valid import ip_addr_valid
 from myfunctions.ip_reach import ip_reach
 from myfunctions.ip_file_valid import ip_file_valid
 from myfunctions.create_threads import create_threads, create_threads1
 from myfunctions.generate_tsf import generate_tsf
-#from myfunctions.automated_bpa import automated_bpa
-
-
-#choice = input("Enter 1 for Stats Dump, Enter 2 for Tech Support File, Enter 3 for fully automated BPA submission (You need a BPA API Token) \nEnter 1,2,3:")
-choice = '2'
-while (choice != '1') and (choice != '2') and (choice != '3'):
-    choice = input("Dude, come one! Enter 1 for Stats Dump and Enter 2 for Tech Support File.\nEnter 1 or 2:")
 
 # Enter username
 firewall_admin = input("Please Enter firewall username: ")
